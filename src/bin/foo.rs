dfdb4979a7809792270821a6071062f275940f7f
themicp <themicp@gmail.com>
2024-12-04 12:21:11 +0000

    Run subscriber, includer, ingestor and distributor on separate threads

--- src/bin/foo.rs
+++ src/bin/foo.rs
@@ -1,49 +1,79 @@
+use std::sync::Arc;
+
 use axelar_xrpl_relayer::{
-    queue::Queue,
-    subscriber::{ChainTransaction, Subscriber},
+    distributor::Distributor, gmp_api, ingestor::Ingestor, queue::Queue, subscriber::Subscriber,
+    xrpl_includer::XRPLIncluder,
 };
-use futures::StreamExt;
+use tokio::sync::watch;
 use tracing::{self, Level};
 use tracing_subscriber::FmtSubscriber;
 use xrpl_types::AccountId;
 
 #[tokio::main]
 async fn main() {
-    // a builder for `FmtSubscriber`.
     let subscriber = FmtSubscriber::builder()
-        // all spans/events with a level higher than TRACE (e.g, debug, info, warn, etc.)
-        // will be written to stdout.
-        .with_max_level(Level::TRACE)
-        // completes the builder.
+        .with_max_level(Level::DEBUG)
         .finish();
 
     tracing::subscriber::set_global_default(subscriber).expect("setting default subscriber failed");
 
-    // Create RMQ connection
     let addr = "amqp://127.0.0.1:5672";
-    let q = Queue::new(addr, "name_here").await;
+    let tasks_queue = Arc::new(Queue::new(addr, "tasks").await);
+    let events_queue = Arc::new(Queue::new(addr, "events").await);
+    let gmp_api = Arc::new(gmp_api::GmpApi::new("http://localhost:8080").unwrap());
+    let xrpl_includer = XRPLIncluder::new().await;
+    let (shutdown_tx, shutdown_rx) = watch::channel(false);
 
     let account = AccountId::from_address("rP9iHnCmJcVPtzCwYJjU1fryC2pEcVqDHv").unwrap();
+
     let mut subscriber = Subscriber::new_xrpl("wss://s.devnet.rippletest.net:51233").await;
-    let _ = subscriber.subscribe(account).await;
-    while let Some(tx) = subscriber.transaction_stream().await.next().await {
-        match tx {
-            ChainTransaction::Xrpl(tx_event) => {
-                let event_string = serde_json::to_string(&tx_event).unwrap();
-                q.publish(event_string.as_bytes()).await;
-            }
+    let events_queue_ref = events_queue.clone();
+    let subscriber_handle = tokio::spawn({
+        let shutdown_rx = shutdown_rx.clone();
+        async move {
+            subscriber
+                .run(account.to_address(), events_queue_ref, shutdown_rx)
+                .await;
+        }
+    });
+
+    let tasks_queue_ref = tasks_queue.clone();
+    let includer_handle = tokio::spawn({
+        let shutdown_rx = shutdown_rx.clone();
+        async move {
+            xrpl_includer.run(tasks_queue_ref, shutdown_rx).await;
+        }
+    });
+
+    let gmp_api_ref = gmp_api.clone();
+    let events_queue_ref = events_queue.clone();
+    let ingestor_handle = tokio::spawn({
+        let shutdown_rx = shutdown_rx.clone();
+        async move {
+            Ingestor::run(gmp_api_ref, events_queue_ref, shutdown_rx).await;
+        }
+    });
+
+    let tasks_queue_ref = tasks_queue.clone();
+    let gmp_api_ref = gmp_api.clone();
+    let distributor_handle = tokio::spawn({
+        let shutdown_rx = shutdown_rx.clone();
+        async move {
+            Distributor::run(gmp_api_ref, tasks_queue_ref, shutdown_rx).await;
+        }
+    });
+
+    tokio::select! {
+        _ = tokio::signal::ctrl_c() => {
+            println!("Shutting down...");
+            let _ = shutdown_tx.send(true);
         }
     }
 
-    // let consumer = q.consumer().await.unwrap();
-    // while let Some(delivery) = consumer.next().await {
-    //     let delivery = delivery.expect("error in consumer");
-    //     info!("Received delivery: {:?}", delivery);
-    //     let data = delivery.data.clone();
-    //     info!(
-    //         "Actual message: {:?}",
-    //         std::str::from_utf8(&data).expect("utf8")
-    //     );
-    //     delivery.ack(BasicAckOptions::default()).await.expect("ack");
-    // }
+    let _ = tokio::join!(
+        subscriber_handle,
+        includer_handle,
+        ingestor_handle,
+        distributor_handle
+    );
 }
