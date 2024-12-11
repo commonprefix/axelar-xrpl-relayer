5fc55b979d9b6da0382cbed7769504421705c8a8
themicp <themicp@gmail.com>
2024-12-11 15:24:44 +0000

    Update task parser to support expected events

--- src/utils.rs
+++ src/utils.rs
@@ -3,14 +3,36 @@ use tracing::{info, warn};
 
 use crate::{
     error::GmpApiError,
-    gmp_types::{CommonTaskFields, ExecuteTask, RefundTask, Task},
+    gmp_types::{
+        CommonTaskFields, ConstructProofTask, ExecuteTask, GatewayTxTask, ReactToWasmEventTask,
+        RefundTask, Task, VerifyTask,
+    },
 };
 
 pub fn parse_task(task_json: &Value) -> Result<Task, GmpApiError> {
     let task_headers: CommonTaskFields = serde_json::from_value(task_json.clone())
         .map_err(|e| GmpApiError::InvalidResponse(e.to_string()))?;
 
+    // TODO: DRY
     match task_headers.r#type.as_str() {
+        "CONSTRUCT_PROOF" => {
+            let task: ConstructProofTask = serde_json::from_value(task_json.clone())
+                .map_err(|e| GmpApiError::InvalidResponse(e.to_string()))?;
+            info!("Parsed ConstructProof task: {:?}", task);
+            Ok(Task::ConstructProof(task))
+        }
+        "GATEWAY_TX" => {
+            let task: GatewayTxTask = serde_json::from_value(task_json.clone())
+                .map_err(|e| GmpApiError::InvalidResponse(e.to_string()))?;
+            info!("Parsed GatewayTx task: {:?}", task);
+            Ok(Task::GatewayTx(task))
+        }
+        "VERIFY" => {
+            let task: VerifyTask = serde_json::from_value(task_json.clone())
+                .map_err(|e| GmpApiError::InvalidResponse(e.to_string()))?;
+            info!("Parsed VerifyTask task: {:?}", task);
+            Ok(Task::Verify(task))
+        }
         "EXECUTE" => {
             let task: ExecuteTask = serde_json::from_value(task_json.clone())
                 .map_err(|e| GmpApiError::InvalidResponse(e.to_string()))?;
@@ -22,6 +44,12 @@ pub fn parse_task(task_json: &Value) -> Result<Task, GmpApiError> {
                 .map_err(|e| GmpApiError::InvalidResponse(e.to_string()))?;
             info!("Parsed Refund task: {:?}", task);
             Ok(Task::Refund(task))
+        }
+        "REACT_TO_WASM_EVENT" => {
+            let task: ReactToWasmEventTask = serde_json::from_value(task_json.clone())
+                .map_err(|e| GmpApiError::InvalidResponse(e.to_string()))?;
+            info!("Parsed ReactToWasmEvent task: {:?}", task);
+            Ok(Task::ReactToWasmEvent(task))
         }
         _ => {
             warn!("Unknown task type: {:?}", task_headers.r#type);
