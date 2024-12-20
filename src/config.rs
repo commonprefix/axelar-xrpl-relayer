80954c0ff804c1f0173deb6777d18a25cabf6bba
themicp <themicp@gmail.com>
2024-12-20 13:58:13 +0000

    Add PAYLOAD_CACHE in config

--- src/config.rs
+++ src/config.rs
@@ -8,6 +8,7 @@ pub struct Config {
     pub includer_secret: String,
     pub queue_address: String,
     pub gmp_api_url: String,
+    pub payload_cache: String,
     pub xrpl_rpc: String,
     pub multisig_address: String,
     pub xrpl_gateway_address: String,
@@ -22,6 +23,7 @@ impl Config {
             includer_secret: env::var("INCLUDER_SECRET").context("Missing INCLUDER_SECRET")?,
             queue_address: env::var("QUEUE_ADDRESS").context("Missing QUEUE_ADDRESS")?,
             gmp_api_url: env::var("GMP_API").context("Missing GMP_API")?,
+            payload_cache: env::var("PAYLOAD_CACHE").context("Missing PAYLOAD_CACHE")?,
             xrpl_rpc: env::var("XRPL_RPC").context("Missing XRPL_RPC")?,
             multisig_address: env::var("MULTISIG_ADDRESS").context("Missing MULTISIG_ADDRESS")?,
             xrpl_gateway_address: env::var("XRPL_GATEWAY_ADDRESS")
