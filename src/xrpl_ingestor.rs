31ce44e16284886d50c649716566ad2d9c33cfb8
themicp <themicp@gmail.com>
2024-12-23 13:08:02 +0000

    properly parse payload hex

--- src/xrpl_ingestor.rs
+++ src/xrpl_ingestor.rs
@@ -510,13 +510,13 @@ impl XrplIngestor {
         let execute_msg = xrpl_multisig_prover::msg::ExecuteMsg::ConstructProof {
             cc_id: CrossChainId::new(task.task.message.source_chain, task.task.message.message_id)
                 .unwrap(),
-            payload: task.task.payload.as_bytes().try_into().unwrap(),
+            payload: hex::decode(task.task.payload).unwrap().into(),
         };
 
         let request = BroadcastRequest::Generic(serde_json::to_value(&execute_msg).unwrap());
         Ok(self
             .gmp_api
-            .post_broadcast(self.config.multisig_prover_address.clone(), &request)
+            .post_broadcast(self.config.xrpl_multisig_prover_address.clone(), &request)
             .await
             .map_err(|e| {
                 IngestorError::GenericError(format!(
