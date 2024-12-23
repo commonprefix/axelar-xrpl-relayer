4eb1f4a5088607977ff0c7ac13973030de97a2a9
themicp <themicp@gmail.com>
2024-12-23 17:10:58 +0000

    Export ticket_creator

--- src/xrpl/mod.rs
+++ src/xrpl/mod.rs
@@ -1,7 +1,9 @@
 mod includer;
 mod ingestor;
 mod subscriber;
+mod ticket_creator;
 
 pub use includer::XrplIncluder;
 pub use ingestor::XrplIngestor;
 pub use subscriber::XrplSubscriber;
+pub use ticket_creator::XrplTicketCreator;
