861b4ebdb83e99cbecbb2719a398fcc9179ce9c8
themicp <themicp@gmail.com>
2024-12-27 14:51:14 +0000

    Lint utils.rs

--- src/utils.rs
+++ src/utils.rs
@@ -1,3 +1,4 @@
+use serde::de::DeserializeOwned;
 use serde_json::Value;
 use tracing::{info, warn};
 use xrpl_api::Memo;
@@ -10,6 +11,10 @@ use crate::{
     },
 };
 
+fn parse_as<T: DeserializeOwned>(value: &Value) -> Result<T, GmpApiError> {
+    serde_json::from_value(value.clone()).map_err(|e| GmpApiError::InvalidResponse(e.to_string()))
+}
+
 pub fn parse_task(task_json: &Value) -> Result<Task, GmpApiError> {
     let task_headers: CommonTaskFields = serde_json::from_value(task_json.clone())
         .map_err(|e| GmpApiError::InvalidResponse(e.to_string()))?;
@@ -17,38 +22,32 @@ pub fn parse_task(task_json: &Value) -> Result<Task, GmpApiError> {
     // TODO: DRY
     match task_headers.r#type.as_str() {
         "CONSTRUCT_PROOF" => {
-            let task: ConstructProofTask = serde_json::from_value(task_json.clone())
-                .map_err(|e| GmpApiError::InvalidResponse(e.to_string()))?;
+            let task: ConstructProofTask = parse_as(task_json)?;
             info!("Parsed ConstructProof task: {:?}", task);
             Ok(Task::ConstructProof(task))
         }
         "GATEWAY_TX" => {
-            let task: GatewayTxTask = serde_json::from_value(task_json.clone())
-                .map_err(|e| GmpApiError::InvalidResponse(e.to_string()))?;
+            let task: GatewayTxTask = parse_as(task_json)?;
             info!("Parsed GatewayTx task: {:?}", task);
             Ok(Task::GatewayTx(task))
         }
         "VERIFY" => {
-            let task: VerifyTask = serde_json::from_value(task_json.clone())
-                .map_err(|e| GmpApiError::InvalidResponse(e.to_string()))?;
+            let task: VerifyTask = parse_as(task_json)?;
             info!("Parsed VerifyTask task: {:?}", task);
             Ok(Task::Verify(task))
         }
         "EXECUTE" => {
-            let task: ExecuteTask = serde_json::from_value(task_json.clone())
-                .map_err(|e| GmpApiError::InvalidResponse(e.to_string()))?;
+            let task: ExecuteTask = parse_as(task_json)?;
             info!("Parsed Execute task: {:?}", task);
             Ok(Task::Execute(task))
         }
         "REFUND" => {
-            let task: RefundTask = serde_json::from_value(task_json.clone())
-                .map_err(|e| GmpApiError::InvalidResponse(e.to_string()))?;
+            let task: RefundTask = parse_as(task_json)?;
             info!("Parsed Refund task: {:?}", task);
             Ok(Task::Refund(task))
         }
         "REACT_TO_WASM_EVENT" => {
-            let task: ReactToWasmEventTask = serde_json::from_value(task_json.clone())
-                .map_err(|e| GmpApiError::InvalidResponse(e.to_string()))?;
+            let task: ReactToWasmEventTask = parse_as(task_json)?;
             info!("Parsed ReactToWasmEvent task: {:?}", task);
             Ok(Task::ReactToWasmEvent(task))
         }
@@ -65,15 +64,20 @@ pub fn extract_from_xrpl_memo(
     memos: Option<Vec<Memo>>,
     memo_type: &str,
 ) -> Result<String, anyhow::Error> {
-    let memos = memos.clone().ok_or(anyhow::anyhow!("No memos"))?;
+    let memos = memos.ok_or_else(|| anyhow::anyhow!("No memos"))?;
+    let desired_type_hex = hex::encode(memo_type).to_lowercase();
 
-    for memo in memos.iter() {
-        if memo.memo_type.is_some()
-            && memo.memo_type.clone().unwrap().to_lowercase()
-                == hex::encode(memo_type).to_lowercase()
-        {
-            return Ok(memo.memo_data.clone().unwrap());
-        }
+    if let Some(memo) = memos.into_iter().find(|m| {
+        m.memo_type
+            .as_ref()
+            .map(|t| t.to_lowercase())
+            .unwrap_or_default()
+            == desired_type_hex
+    }) {
+        Ok(memo
+            .memo_data
+            .ok_or_else(|| anyhow::anyhow!("memo_data is missing"))?)
+    } else {
+        Err(anyhow::anyhow!("No memo with type: {}", memo_type))
     }
-    Err(anyhow::anyhow!("No memo with type: {}", memo_type))
 }
